b6c24960b821dda0f9c93a101a97df8ca2b7dc5b
GVK <venkatakousikcse01@gmail.com>
2025-03-21 13:23:33 +0530

    feat: backend changes

--- models/othersModel.js
+++ models/othersModel.js
@@ -4,24 +4,20 @@ const othersSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   Activities: [{
     activityDetails: { type: String, },
-    UploadFiles: [{ Image: { type: String } }]
   }],
   Responsibilities: [{
     Responsibility: { type: String, },
     AssignedBy: { type: String, },
-    UploadFiles: [{ Image: { type: String } }]
   }],
   Contribution: [{
     contributionDetails: { type: String, },
     Benefit: { type: String, },
-    UploadFiles: [{ Image: { type: String } }]
   }],
   Awards: [{
     Award: { type: String, },
     AwardedBy: { type: String, },
     Level: { type: String, },
     Description: { type: String, },
-    UploadFiles: [{ Image: { type: String } }]
   }],
 })
 
